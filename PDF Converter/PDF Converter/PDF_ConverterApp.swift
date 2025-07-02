efea7cb89fa3b4a9418f750bc7adad527c1ed37c
Oleg Kalistratov <159171465+solveit-Oleg-Kalistratov@users.noreply.github.com>
2025-07-02 14:27:34 +0300

    init project

---
+++ PDF Converter/PDF Converter/PDF_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ConverterApp.swift
+//  PDF Converter
+//
+//  Created by Oleg on 2.07.25.
+//
+
+import SwiftUI
+
+@main
+struct PDF_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
